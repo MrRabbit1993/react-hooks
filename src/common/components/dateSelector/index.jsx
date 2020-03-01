4314d5c62dfde836af78e32b8c8213810ccbc3be
兔子先森 <1993iweb@gmail.com>
2020-03-01 22:32:45 +0800

    月份组件

--- src/common/components/dateSelector/index.jsx
+++ src/common/components/dateSelector/index.jsx
@@ -3,14 +3,32 @@ import Proptypes from "prop-types";
 import classnames from "classnames";
 import "./index.css";
 import Header from "./../Header";
+import Month from "./../month";
 function DateSelector(props) {
     const { show, onSelect, onBack } = props;
+    const now = new Date();
+    now.setHours(0);
+    now.setMinutes(0);
+    now.setSeconds(0);
+    now.setMilliseconds(0);
+    now.setDate(1);
+    const monthSequence = [now.getTime()];
+    now.setMonth(now.getMonth() + 1);
+    monthSequence.push(now.getTime());
+    now.setMonth(now.getMonth() + 1);
+    monthSequence.push(now.getTime());
     return (
         <div className={classnames('date-selector', {
             hidden: !show
         })}>
             <Header title="日期选择" onBack={onBack} />
-            <div className="date-selector-tables"></div>
+            <div className="date-selector-tables">
+                {
+                    monthSequence.map(month => {
+                        return <Month key={month} startingTimeInMonth={month} onSelect={onSelect} />
+                    })
+                }
+            </div>
         </div>
     )
 }
---
+++ src/common/components/month/index.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Proptypes from "prop-types";
+
+function Month(props) {
+    const { startingTimeInMonth, onSelect } = props;
+    const startDay = new Date(startingTimeInMonth);
+    const currentDay = new Date(startingTimeInMonth);
+    let days = [];
+    while (currentDay.getMonth() === startDay.getMonth()) {
+        days.push(currentDay.getTime());
+        currentDay.setDate(currentDay.getDate() + 1)
+    }
+    //补齐前面 日期，如果是周日，前面就需要补齐6个
+    days = new Array(startDay.getDay() ? startDay.getDay() - 1 : 6).fill(null).concat(days);
+    //补齐后面的
+    const lastDay = new Date(days[days.length - 1]);
+    days = days.concat(new Array(lastDay.getDay() ? 7 - lastDay.getDay() : 0).fill(null));
+    const weeks = [];
+    for (let row = 0; row < days.length; row++) {
+        const week = days.slice(row * 7, (row + 1) * 7);
+        weeks.push(week);
+    }
+    return (
+        <table className="date-table">
+            <thead>
+                <tr>
+                    <td colSpan="7">
+                        <h5>{startDay.getFullYear()}年{startDay.getMonth()}</h5>
+                    </td>
+                </tr>
+            </thead>
+            <tbody class="date-table-weeks">
+                <th>周一</th>
+                <th>周二</th>
+                <th>周三</th>
+                <th>周四</th>
+                <th>周五</th>
+                <th className="weekend">周六</th>
+                <th className="weekend">周日</th>
+            </tbody>
+        </table>
+    )
+}
+Month.propTypes = {
+    startingTimeInMonth: Proptypes.number.isRequired,
+    onSelect: Proptypes.func.isRequired
+}
+export default Month;
