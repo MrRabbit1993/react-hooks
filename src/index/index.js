ee2e73fdf53ce1001af37a96c569d4a020144903
MrRabbit1993 <1993iweb@gamil.com>
2020-03-09 17:39:37 +0800

    加入 serviceWorker

--- src/index/index.js
+++ src/index/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
-
+import * as serviceWorker from './../serviceWorker';
 import 'normalize.css';
 import store from './redux/store';
 import './index.css';
@@ -12,3 +12,8 @@ ReactDom.render(
     </Provider>,
     document.getElementById('root')
 );
+if (process.env.NODE_ENV === 'production') {
+    serviceWorker.register();
+} else {
+    serviceWorker.unregister();
+}
